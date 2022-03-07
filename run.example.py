8cc9e036a87151e1947e9104af8f23fd5f462dc7
astrofle <psalas@nrao.edu>
2022-03-07 17:02:00 -0500

    Git clean up

--- run.example.py
+++ run.example.py
@@ -6,7 +6,7 @@ from astropy import units as u
 from groundhog import sd_fits, spectral_axis
 
 from rrlpipe import utils
-from rrlpipe.operations import calibrate, rfi_flag, stack
+from rrlpipe.operations import calibrate, rfi_flag, stack, find_gh
 
 
 rod_dict = {'Ra': 'RALongMap',
@@ -41,6 +41,8 @@ def pipeline(filein, outdir, ifnums, plnums, rod, vbank, settings):
         print("Loading Tcal values.")
         tcal_arr = np.load(settings.tcal_file)
 
+    continuum_models = settings.continuum_models
+
     proc_files = []
     # Loop over spectral window and polarization.
     for ifnum in ifnums:
@@ -52,6 +54,14 @@ def pipeline(filein, outdir, ifnums, plnums, rod, vbank, settings):
             table = sdfits.get_rows(1, map_scans, ifnum=ifnum, plnum=plnum)
             # Get frequency axis.
             freq = spectral_axis.compute_freq_axis(table)
+            freq_cntr = np.nanmedian(freq[0]).to('MHz')
+            print(f'Central frequency: {freq_cntr}')
+            ## Define model map.
+            #cont_mod_list = list(continuum_models.keys())
+            #fmod = cont_mod_list[np.argmin(abs(np.array(cont_mod_list) - freq_cntr.to('MHz').value))]
+            #print(f'Will use the model file: {continuum_models[fmod]}')
+            #gh = find_gh.run(table, continuum_models[fmod])
+
             # Find RRLs.
             print("Finding RRLs.")
             lines = utils.find_rrls(freq[0], settings.line, settings.vel_range, z=settings.z)
@@ -62,8 +72,15 @@ def pipeline(filein, outdir, ifnums, plnums, rod, vbank, settings):
             for n,chans in lines.items():
                 print(f"Processing line: {settings.line} n={n}")
                 table_rrl = utils.split_channel_range(table, chans[0], chans[1], dch=1)
+                freq_rrl = spectral_axis.compute_freq_axis(table_rrl)
+                freq_rrl_cntr = np.nanmedian(freq_rrl[0]).to('MHz')
+                cont_mod_list = list(continuum_models)
+                fmod = cont_mod_list[np.argmin(abs(np.array(cont_mod_list) - freq_cntr.to('MHz').value))]
+                print(f'Will use the model file: {continuum_models[fmod]}')
+                gh = find_gh.run(table_rrl, continuum_models[fmod])
                 print("Calibrating scans.")
-                table_rrl = calibrate.run(table_rrl, settings.cal_poly_order, settings.blank_lines, tcal=tcal)
+                cal_args = (table_rrl, settings.cal_poly_order, settings.blank_lines, gh, tcal)
+                table_rrl = calibrate.run(cal_args, mode='gh')
                 print("Flagging RFI.")
                 rfi_file_path = f"{outdir}/spw_{ifnum}_pol_{plnum}_n_{n}"
                 rfi_file = f"{rfi_file_path}_rfi.fits"
@@ -83,33 +100,31 @@ def pipeline(filein, outdir, ifnums, plnums, rod, vbank, settings):
                 cubefile = utils.freq2vel(cubefile, line=settings.line, z=0, qnidx=0)
                 print(f"Gridded data is in: {cubefile}")
 
-    
-    ## Make a report for the processed lines.
-    #line_vmin = settings.v_center - settings.dv
-    #line_vmax = settings.v_center + settings.dv
-    #line_list_file = f"{outdir}/line_list_{vbank}.txt"
-    #line_list = utils.make_line_list(proc_files, line_list_file, settings.rms_vmin, settings.rms_vmax, line_vmin, line_vmax)
-
 
 if __name__ == "__main__":
     
-    import settings_800_01 as settings
+    #import settings_340 as settings
+    import settings_800_CygX as settings
 
-    project = 'AGBT22A_437'
+    project = 'AGBT21A_292'
+    #sessions = [f'{s:02d}' for s in range(15,24,1)]
+    sessions = [f'{s:02d}' for s in range(1,14,1)]
+    #sessions = ['06']
 
-    for session in ['01']:
-        for rod in ['Dec']:
-            for vbank in ['A', 'B']:
+    for session in sessions:
+        for rod in ['Ra', 'Dec']:
+            for vbank in settings.vbanks.keys():
                 ifnums = settings.vbanks[vbank]
                 plnums = [0,1]
                 projid = f'{project}_{session}'
                 filein = f'/home/sdfits/{projid}/{projid}.raw.vegas/{projid}.raw.vegas.{vbank}.fits'
-                outdir = f'/home/scratch/psalas/projects/GDIGS-Low/data/target-v2/{session}/{rod}/'
-                pipeline(filein, outdir, ifnums, plnums, rod, vbank, settings)
+                outdir = f'/home/scratch/psalas/projects/CygnusX/data/target/hrrl-pipe-v4/{session}/{rod}/'
+                #pipeline(filein, outdir, ifnums, plnums, rod, vbank, settings)
 
             print('Stacking.')
             stack.run(outdir, f'{outdir}/line_list.txt', -300e3*u.m/u.s, 300e3*u.m/u.s, 500*u.m/u.s,
                       settings.rms_vmin*u.m/u.s, settings.rms_vmax*u.m/u.s, 
                       (settings.v_center - settings.dv)*u.m/u.s, 
                       (settings.v_center + settings.dv)*u.m/u.s, 
-                      output='stack.fits')
+                      output='stack.fits', max_deg=settings.max_deg, 
+                      shape=(1201, 198, 237))
--- settings.example.py
+++ settings.example.py
@@ -9,15 +9,15 @@ vbanks = {'A':[0,1,2,3,4,5,6,7],
 # Velocities in meters per second.
 line = 'RRL_HIalpha'
 vel_range = 500e3 # m/s
-z = 3.17e-4
-v_center = 95.e3 # m/s
+z = 0
+v_center = 0.e3 # m/s
 dv = 50e3 # m/s
 
 # Calibration settings.
 cal_poly_order = 11
 blank_lines = {'RRL_HIalpha':[dv,z],
                'RRL_CIalpha':[dv,z]}
-
+tcal_file = '/home/scratch/psalas/projects/CygnusX/data/tcal/tcal_800.npy'
 
 # RFI flagging settings.
 lua_strategy = '/home/scratch/psalas/projects/GDIGS-Low/gbt-rrl-pipeline/strategies/gbt-800.lua'
